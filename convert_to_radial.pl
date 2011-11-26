088813cee2ae698dc1f8683330a5e5609edbf5fb
Peter McLarnan <croikle@gmail.com>
2011-11-26 02:47:17 -0500

    Fixed floating-point and negatives

--- convert_to_radial.pl
+++ convert_to_radial.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl
 # Converts coordinates given in x,y space to coordinates in the bot's r1,r2 space.
 # Truncates to integers.
-# Issues when a number isn't in 123.456 format, specifically scientific notation.
 
 #space between pulleys, in svg pixels
 $width = 1000;
@@ -9,6 +8,6 @@ $width = 1000;
 $scale = 1;
 
 while (<>) {
-	s/(\d*\.?\d*),(\d*\.?\d*)/$r1=sqrt($1**2+$2**2)*$scale; $r2=sqrt(($width-$1)**2+$2**2)*$scale; sprintf("%d,%d", $r1, $r2);/eg;
+	s/(?<x>-?\d*\.?\d*([eE][+-]?\d+)?),(?<y>-?\d*\.?\d*([eE][+-]?\d+)?)/$r1=sqrt($+{x}**2+$+{y}**2)*$scale; $r2=sqrt(($width-$+{x})**2+$+{y}**2)*$scale; sprintf("%d,%d", $r1, $r2);/eg;
 	print;
 }
