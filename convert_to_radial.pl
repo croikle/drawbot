56ce6f1552d51e57a780ca8cb56e0b1d4a07a077
Peter McLarnan <croikle@gmail.com>
2011-11-26 02:22:24 -0500

    Wrote convert_to_radial. First go, but seems to work.

--- convert_to_radial.pl
+++ convert_to_radial.pl
@@ -1,2 +1,12 @@
 #!/usr/bin/perl
 # Converts coordinates given in x,y space to coordinates in the bot's r1,r2 space.
+
+#space between pulleys
+$width = 1000;
+#global scaling factor
+$scale = 1;
+
+while (<>) {
+	s/(\d*\.?\d*),(\d*\.?\d*)/$r1=sqrt($1**2+$2**2)\/$scale; $r2=sqrt(($width-$1)**2+$2**2)\/$scale; sprintf("%d,%d", $r1, $r2);/eg;
+	print;
+}
