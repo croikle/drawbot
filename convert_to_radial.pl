aeb394541cd215529f19de970ea96e0933cadd74
Peter McLarnan <croikle@gmail.com>
2011-11-26 02:36:26 -0500

    Documented more. Changed scaling to multiply rather than divide.

--- convert_to_radial.pl
+++ convert_to_radial.pl
@@ -1,12 +1,14 @@
 #!/usr/bin/perl
 # Converts coordinates given in x,y space to coordinates in the bot's r1,r2 space.
+# Truncates to integers.
+# Issues when a number isn't in 123.456 format, specifically scientific notation.
 
-#space between pulleys
+#space between pulleys, in svg pixels
 $width = 1000;
-#global scaling factor
+#global multiplicative scaling factor
 $scale = 1;
 
 while (<>) {
-	s/(\d*\.?\d*),(\d*\.?\d*)/$r1=sqrt($1**2+$2**2)\/$scale; $r2=sqrt(($width-$1)**2+$2**2)\/$scale; sprintf("%d,%d", $r1, $r2);/eg;
+	s/(\d*\.?\d*),(\d*\.?\d*)/$r1=sqrt($1**2+$2**2)*$scale; $r2=sqrt(($width-$1)**2+$2**2)*$scale; sprintf("%d,%d", $r1, $r2);/eg;
 	print;
 }
