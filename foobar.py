fa545123d7352a104862697d7e9fa6dbac89715f
Nicholas McLarnan <nickm@novgorod.localdomain>
2011-12-25 01:54:33 -0500

    foobar.py now passes input directly by character, without splitting first

--- foobar.py
+++ foobar.py
@@ -6,15 +6,13 @@ import time
 
 s = serial.Serial("/dev/ttyACM0")
 inputread = sys.stdin.read()
-positions = inputread.split()
 
 time.sleep(1)
 
-while len(positions) >= 1:
-	foo = positions.pop(0) + " "
-	s.write(foo)
-	print "foo: \"" + foo + "\""
-	s.read()
-	print "dot read"
+for n in inputread:
+	s.write(n)
+   if (n == '\n' or n == ' '):
+      s.read()
+      print "dot read"
 
 print "All Done"
